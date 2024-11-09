75bda73eeaf9aa3f79bd7c30fe0b07157a457e3a
Mateus Euzébio <mateuseuzebio@hotmail.com>
2024-11-08 22:46:21 -0300

    teste analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Devocionais from './pages/Devocionais';
